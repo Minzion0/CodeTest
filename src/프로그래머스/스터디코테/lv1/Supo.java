d96122da4f231c8d9fd2f074134b5ed7ab55f1a7
minzion <aert12435@gmail.com>
2023-11-21 16:24:27 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/42840

--- src/프로그래머스/스터디코테/lv1/Supo.java
+++ src/프로그래머스/스터디코테/lv1/Supo.java
@@ -16,30 +16,36 @@ package 프로그래머스.스터디코테.lv1;
  * 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
  */
 public class Supo {
-    public int[] solution(int[] answers) {
+    public static int[] solution(int[] answers) {
         int[] answer = {};
+        // 수포자별 반복 패턴
         int[] su1= {1, 2, 3, 4, 5};
         int[] su2= {2, 1, 2, 3, 2, 4, 2, 5};
         int[] su3= {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int sur1=0,sur2=0,sur3=0;
+
+        //인덱스 값 저장
         int sui1=0,sui2=0,sui3=0;
+        // 수포자별 정답 횟수 저장
+        int[] rArray= {0,0,0};
+
         for(int i=0; i<answers.length;i++){
-            int result = answer[i];
+            int result = answers[i];
 
             if (result==su1[sui1]){
-                sur1++;
+                rArray[0]=rArray[0]+1;
             }
 
             if (result==su2[sui2]){
-                sur2++;
+                rArray[1]=rArray[1]+1;
             }
 
             if (result==su3[sui3]){
-                sur3++;
+                rArray[2]=rArray[2]+1;
             }
 
             sui1++; sui2++; sui3++;
 
+            //수포자별 패턴 반복용 인덱스값 초기화 실시
             if (sui1>su1.length-1){
                 sui1=0;
             }
@@ -54,6 +60,41 @@ public class Supo {
 
 
         }
+
+        int max=0;
+        // 수포자별 정답 횟수 확인 및 최대 정답수 확인
+        for (int i : rArray) {
+            if (max<=i){
+                max=i;
+            }
+
+        }
+        int[] temp=new int[0];
+        // 최다 득점자 확인
+        for (int i = 0; i < rArray.length; i++) {
+            if (max==rArray[i]){
+                temp= new int[answer.length+1];
+                if (answer.length!=0){
+                    for (int j = 0; j < answer.length; j++) {
+                        temp[j]=answer[j];
+                    }
+                }
+
+                temp[answer.length]=i+1;
+            }
+
+
+            answer=temp;
+        }
+
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] a= {1,3,2,4,2};
+        int[] solution = solution(a);
+
+        System.out.println("solution = " + solution[0]);
     }
 }
